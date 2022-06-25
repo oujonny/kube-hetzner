29c0f18b1b6a98e00585285e6a5488d1ffb203da
jonny <jonny@immerda.ch>
2022-06-25 15:12:02 +0200

    fix var validation

--- variables.tf
+++ variables.tf
@@ -31,7 +31,7 @@ variable "network_region" {
   description = "Default region for network"
   type        = string
   validation {
-    condition = "eu-central" || "us-east"
+    condition = var.network_region == "eu-central" || var.network_region == "us-east"
     error_message = "The network region must be either eu-central or us-east. See: https://docs.hetzner.com/cloud/general/locations"
   }
   default     = "eu-central"
